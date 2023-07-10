ce68e6917bb3be6c840807bc075ed9b7da15c04c
Christian64 <christianabreuh@gmail.com>
2023-07-10 11:27:15 -0400

    Adding twitterClone project.

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "chVideos",
     description: "Download Instagram Reels video by a Telegram Bot",
     sourceCode: "https://github.com/Christian64/chVideos",
-    link: "https://github.com/Christian64/chVideos",
+    link: "https://t.me/chvideodownloaderbot",
     thumbnail: "./chVideos.png",
   },
   {
@@ -17,14 +17,7 @@ export const projects = [
     title: "FormulaAi",
     description: "Create excel & GSheets formulas with AI	",
     sourceCode: "https://www.google.com",
-    link: "https://www.google.com",
-    thumbnail: "https://getuikit.com/v2/docs/images/placeholder_600x400.svg",
-  },
-  {
-    title: "EnlaceBio",
-    description: "create a linkbio for your social media",
-    sourceCode: "https://www.google.com",
-    link: "https://www.google.com",
-    thumbnail: "https://getuikit.com/v2/docs/images/placeholder_600x400.svg",
+    link: "https://github.com/Christian64/twitter-nodejs-express-mongodb-mongoose-clone",
+    thumbnail: "./twitterClone.png",
   },
 ];
