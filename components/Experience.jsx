a33d64611a64bd9b9e0b2117fe09cb9a72dae1cf
Christian64 <christianabreuh@gmail.com>
2024-06-28 10:44:35 -0400

    Making experiences & Projects responsive

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "usehooks-ts";
 export const Experience = ({
   company,
   time,
@@ -7,26 +8,27 @@ export const Experience = ({
   description,
   skills,
 }) => {
+  const matches = useMediaQuery("(min-width: 768px)");
   return (
     <a
       href={website}
-      className="card bg-base-100 shadow-md w-full font-roboto"
+      className="card card-compact lg:card-normal bg-base-100 shadow-md w-full font-roboto"
       target="_blank"
     >
-      <div className="card-body p-5 lg:gap-5 flex flex-row items-start">
+      <div className="card-body  p-5 lg:gap-5 flex flex-row items-start">
         <div className="avatar">
           <div className="w-10 lg:w-12 rounded-xl flex gap-2">
             <img src={logo} alt={company} />
           </div>
         </div>
         <div>
-          <h3 className="font-bold">
-            {position}
-            <small className="font-normal text-md">
-              - <span>{company}</span> ({time})
-            </small>
-          </h3>
-          <p>{description}</p>
+          <h3 className="text-sm font-bold">{position}</h3>
+          <small className="font-normal text-md">
+            {company} - {time}
+          </small>
+          <p className="hidden lg:block text-ellipsis max-h-[30px]">
+            {description}
+          </p>
         </div>
         <div className="card-footer truncate font-bold">{skills}</div>
       </div>
--- components/Project.jsx
+++ components/Project.jsx
@@ -8,15 +8,22 @@ export const Project = ({ name, link, img, description, github }) => {
         <div href="link" className="card-body">
           <div className="flex justify-between">
             <h3 className="card-title font-ptSerif">{name}</h3>
-            <div className="flex gap-4">
-              <a href={link} className="btn btn-secondary font-normal btn-sm">
-                <EyeIcon />
-                See Project
-              </a>
-              <a href={github} className="flex items-center">
-                <GithubIcon />
-                Source Code
-              </a>
+            <div className="flex lg:gap-4">
+              {link && (
+                <a
+                  href={link}
+                  className="btn btn-ghost lg:btn-primary font-normal btn-sm"
+                >
+                  <EyeIcon />
+                  <span className="hidden lg:inline">See Project</span>
+                </a>
+              )}
+              {github && (
+                <a href={github} className="btn btn-ghost font-normal btn-sm">
+                  <GithubIcon />
+                  <span className="hidden lg:inline">Source Code</span>
+                </a>
+              )}
             </div>
           </div>
           <p className="text-md">{description}</p>
